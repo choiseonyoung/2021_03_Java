48bf2ceb6f4b671c7dce8f0fe5a8c8ed04c74750
choiseonyoung <bviibb@naver.com>
2021-03-17 11:50:29 +0900

    Values

---
+++ Java_001_Value/src/com/callor/values/Values_02.java
@@ -0,0 +1,9 @@
+package com.callor.values;
+
+public class Values_02 {
+	public static void main(String[] args) {
+		System.out.println("반지름이 23인 원의");
+		System.out.println("둘레 : " + 23 * 2 * 3.14);
+		System.out.println("넓이 : " + 23 * 23 * 3.14 );
+	}
+}
