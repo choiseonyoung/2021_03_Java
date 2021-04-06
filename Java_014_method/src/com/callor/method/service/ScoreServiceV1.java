0d9edd4fb4c1bfb9533f40be5fd9251c0d0c7978
choiseonyoung <bviibb@naver.com>
2021-04-06 16:43:41 +0900

    Method

---
+++ Java_014_method/src/com/callor/method/service/ScoreServiceV1.java
@@ -0,0 +1,10 @@
+package com.callor.method.service;
+
+public class ScoreServiceV1 {
+
+	public static void main(String[] args) {
+		InputServiceV1 isV1 = new InputServiceV1();
+		
+		isV1.inputScore();
+	}
+}
